4a7701f75ac4897f34ab6c1dae1d228a60c1420f
kolykonon <@kononikhinnikolay@gmail.com>
2026-01-14 19:58:11 +0300

    Добавил модель задания

--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('prog', 'В процессе'),
+        ('cancel', 'Отменена'),
+        ('complete', 'Завершена'),
+
+    ]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    add_date = models.DateTimeField(auto_now_add=True)
+    goal_date = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='prog')
+    important = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['add_date', 'goal_date', 'status']
